3e81d6fbcc426c835c2cd56f940c7bb94472f1c4
kkh061101 <kkh061101@naver.com>
2022-12-19 12:07:30 +0900

    수정

--- carom/src/Components/Navigation.js
+++ carom/src/Components/Navigation.js
@@ -2,13 +2,10 @@ import React from "react";
 import './Navigation.scss';
 
 function Navigation() {
-  //asdfsadf
-  return( <nav className="Navigation">
+  return (<nav className="Navigation">
     <h1>Carom</h1>
     <ul>
-      <h1>Carom</h1>
       <li>
-        <img width={'40px'} height={'40px'} src="https://cdn4.iconfinder.com/data/icons/eon-ecommerce-i-1/32/home_house_desktop_dashboard-256.png" title="home" alt="home" />
         <span>Home</span>
       </li>
       <li>
@@ -20,14 +17,11 @@ function Navigation() {
       <li>
         <span><a href="http://www.danggubaksa.com/home/billiards-basic/billiard-rules/" target={"_blank"} rel='noonpener noreferrer'>당구 규칙</a></span>
       </li>
-      <span id="user_icon">
-        <img width={'40px'} src="https://cdn4.iconfinder.com/data/icons/eon-ecommerce-i-1/32/user_profile_man-512.png" alt="user_icon" />
-      </span>
     </ul>
 
     <div className="log-in">
-         <button>로그인</button>
-         <button>회원가입</button>
+      <button>로그인</button>
+      <button>회원가입</button>
     </div>
 
   </nav>);
