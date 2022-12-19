abf303a96a194b90945bff366c043921a0161d6b
이동욱 <106513119+donguk1207@users.noreply.github.com>
2022-12-19 11:28:03 +0900

    내비게이션 추가

--- carom/src/Components/Navigation.js
+++ carom/src/Components/Navigation.js
@@ -2,20 +2,20 @@ import React from "react";
 import './Navigation.scss';
 
 function Navigation() {
-  return <nav className="Navigation">
+  return( <nav className="Navigation">
+    <h1>Carom</h1>
     <ul>
-      <h1>Carom</h1>
-      <li>
-        <img title="home" alt="home" /><span>home</span>
-      </li>
-      <li>
-        <span>이용규정</span>
-      </li>
-      <li>
-        <span>신청목록</span>
-      </li>
+        <li>Home</li>
+        <li>이용규정</li>
+        <li>신청목록</li>
+        <li><a href="http://www.danggubaksa.com/home/billiards-basic/billiard-rules/">당구규칙</a></li>
     </ul>
-  </nav>;
+
+    <ul className="navbar__icons">
+         <li><img width={'40px'} src="https://cdn1.iconfinder.com/data/icons/iconoir-vol-3/24/log-out-256.png"/></li>
+    </ul>
+
+  </nav>);
 }
 
 export default Navigation;
