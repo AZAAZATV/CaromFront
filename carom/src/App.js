c4424b65e16450d9f00dc601ffbf2ab967c4af9b
kkh061101 <kkh061101@naver.com>
2022-12-19 14:17:01 +0900

    이용약관 추가

--- carom/src/App.js
+++ carom/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navigation from "./Components/Navigation";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
+import TermsOfUse from "./Components/TermsOfUse";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navigation />
         <Routes>
           <Route path="/home" element={<Home />} />
+          <Route path="/Terms-of-use" element={<TermsOfUse />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ carom/src/Components/TermsOfUse.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './TermsOfUse.scss';
+
+function TermsOfUse() {
+  return <div className="TermsOfUse">
+    와 샌즈
+  </div>;
+}
+
+export default TermsOfUse;
