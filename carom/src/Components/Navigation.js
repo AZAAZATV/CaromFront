87de8995d623ab074fbf5b5db006848e6eb9d615
kkh061101 <kkh061101@naver.com>
2022-12-19 13:36:00 +0900

    업뎃

--- carom/src/Components/Navigation.js
+++ carom/src/Components/Navigation.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Navigation.scss';
 
 function Navigation() {
-  return (<nav className="Navigation">
+  return <nav className="Navigation">
     <h1>Carom</h1>
     <ul>
       <li>
-        <span>Home</span>
+        <span><Link to="/home">Home</Link></span>
       </li>
       <li>
-        <span>이용규정</span>
+        <span><Link to='/'>이용규정</Link></span>
       </li>
       <li>
         <span>신청목록</span>
@@ -18,13 +19,11 @@ function Navigation() {
         <span><a href="http://www.danggubaksa.com/home/billiards-basic/billiard-rules/" target={"_blank"} rel='noonpener noreferrer'>당구 규칙</a></span>
       </li>
     </ul>
-
     <div className="log-in">
       <button>로그인</button>
       <button>회원가입</button>
     </div>
-
-  </nav>);
+  </nav>;
 }
 
 export default Navigation;
