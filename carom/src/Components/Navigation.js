5b5f0f44c5d0986ee1ae7129bd2143e4dc41ddb8
kkh061101 <kkh061101@naver.com>
2022-12-19 10:13:15 +0900

    당구 규정 추가

--- carom/src/Components/Navigation.js
+++ carom/src/Components/Navigation.js
@@ -14,6 +14,9 @@ function Navigation() {
       <li>
         <span>신청목록</span>
       </li>
+      <li>
+        <span><a href="http://www.danggubaksa.com/home/billiards-basic/billiard-rules/" target={"_blank"} rel='noonpener noreferrer'>당구 규칙</a></span>
+      </li>
     </ul>
   </nav>;
 }
