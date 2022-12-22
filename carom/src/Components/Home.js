ddba38012c2cc8411154345bb2048e57e609ad55
kkh061101 <kkh061101@naver.com>
2022-12-22 20:40:44 +0900

    Update Home.js

--- carom/src/Components/Home.js
+++ carom/src/Components/Home.js
@@ -5,15 +5,21 @@ import 'moment/locale/ko';
 
 function Home() {
   const [clock, setClock] = useState();
+  const [sub1, setSub1] = useState(false);
+  const [can1, setCan1] = useState(false);
+  const [sub2, setSub2] = useState(false);
+  const [can2, setCan2] = useState(false);
   useEffect(() => {
     const id = setInterval(() => {
       setClock(moment().format('hh:mm:ss'));
     }, 1);
     return (() => clearInterval(id));
   }, []);
+  useEffect(() => {
+
+  }, []);
   return <div className="Home">
     <div className="main">
-
       <div className="clockbox">
         <div className="clock">
           {clock}
@@ -22,7 +28,6 @@ function Home() {
 
       <div className="alert">
         <h2>공지사항</h2>
-
         <div className="alertbox">
           ??
         </div>
@@ -34,13 +39,13 @@ function Home() {
         <div className="applybox1">
           <p>1팀</p>
           <h2>0/4</h2>
-          <button>신청하기</button>
+          {!can1 ? (!sub1 ? <button onClick={() => setSub1(true)}>신청하기</button> : <button onClick={() => setCan1(true)}>신청취소</button>) : <button>신청 불가</button>}
         </div>
 
         <div className="applybox2">
           <p>2팀</p>
           <h2>0/4</h2>
-          <button>신청하기</button>
+          {!can2 ? (!sub2 ? <button onClick={() => setSub2(true)}>신청하기</button> : <button onClick={() => setCan2(true)}>신청취소</button>) : <button>신청 불가</button>}
         </div>
 
         <div className="rulebox">
