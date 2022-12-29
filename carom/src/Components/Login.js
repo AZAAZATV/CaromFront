7a0922da98c0cdd542dfaee36533542e8a5a196e
kkh061101 <kkh061101@naver.com>
2022-12-29 12:06:05 +0900

    Update Login.js

--- carom/src/Components/Login.js
+++ carom/src/Components/Login.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Login.scss';
 import axios from 'axios';
+import moment from 'moment';
+import 'moment/locale/ko';
 import { Link } from 'react-router-dom';
 
 function Login() {
   const [id, setId] = useState();
   const [password, setPassword] = useState();
+  const [time, setTime] = useState();
+  useEffect(() => {
+    setInterval(() => {
+      setTime(moment().format('hh:mm:ss'));
+    }, 100);
+  }, []);
   const tryLogin = async () => {
     // localStorage.setItem('id', "asdf");
     // localStorage.setItem('name', "asdf");
@@ -20,7 +28,12 @@ function Login() {
         }),
         headers: { 'Content-Type': `application/json`, 'withCredentials': 'true', 'Access-Control-Allow-Origin': '*' }
       });
-      console.log(data);
+      if (data.data === 'OK') {
+        console.log('성공');
+      }
+      else {
+        console.log('아이디 또는 비밀번호를 잘못 입력했습니다.');
+      }
     } catch (e) {
       console.log(e);
     }
@@ -56,6 +69,7 @@ function Login() {
           tryLogin();
         }}>로그인</button>
       </div>
+
       <div className="make">
         <div className="nosign">아직계정이 없으신가요?</div>
         <Link to='/signup'>
